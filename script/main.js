19f7027c69a151834f1e5b6d11ebdc82a96bac69
XSendy <sendisaputrayahu@gmail.com>
2025-04-06 00:39:45 +0700

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,120 @@
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+    if ((e.ctrlKey || e.metaKey) && (e.key === 'c' || e.key === 'C')) {
+        e.preventDefault();
+    }
+});
+
+const messages = [
+    "Tap dimana aja yaaa",
+    "hey imuppp ૮₍ ˶ᵔ ᵕ ᵔ˶ ₎ა",
+    "i want say something",
+    "before you came, my life was black and white, but since you are here, my life is colorful. thank you for coming into my life and being present in my life, I am lucky to meet a woman as good as you. It will be endless if I tell you a million things that I like about you because you are too perfect to explain, I love you so much and I hope you know that.",
+    "pencet lagi",
+    "you are worth it always have been, always will you be. Worth more than.a billion galaxies ⸜(｡˃ ᵕ ˂ )⸝♡",
+    "i loveeee u, i loveeee u everyday, i loveeee u every hour, i loveeee u every minute, i loveeee u every second, i loveeee u to the moon and back, i loveeee u the bottom of sea, i loveeee u every universe, i loveeee u more than, i lovee myself, i loveeee u more and more and u're the only one love ₍ᐢ. .ᐢ₎ ₊˚⊹♡",
+    "akuuu gatauu mww bilang apalagii imupp",
+    "terimakasiii yaaa imuppp",
+    "aku cuma mau bilang",
+    "ikan cucut nabrak curutt",
+    "i love you imuppp ฅ^._.^ฅ",
+    "bjiirlah",
+    "coba pencet tombol dibawah ini imuppp 💝"
+];
+
+let currentPage = 0;
+let isLastPage = false;
+
+function showMessage() {
+    $('.message').text(messages[currentPage]);
+    
+    isLastPage = currentPage === messages.length - 1;
+    
+    if (isLastPage) {
+        $('.next-button').show();
+        $('.bg_heart').css('cursor', 'default');
+    } else {
+        $('.next-button').hide();
+        $('.bg_heart').css('cursor', 'pointer');
+    }
+}
+
+$('.bg_heart').on('click', function() {
+    if (!isLastPage) {
+        currentPage++;
+        showMessage();
+    }
+});
+
+var love = setInterval(function() {
+    var r_num = Math.floor(Math.random() * 40) + 1;
+    var r_size = Math.floor(Math.random() * 65) + 10;
+    var r_left = Math.floor(Math.random() * 100) + 1;
+    var r_bg = Math.floor(Math.random() * 25) + 100;
+    var r_time = Math.floor(Math.random() * 5) + 5;
+    
+    $('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);animation:love " + r_time + "s ease'></div>");
+    
+    $('.bg_heart').append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);animation:love " + (r_time + 5) + "s ease'></div>");
+    
+    $('.heart').each(function() {
+        var top = parseFloat($(this).css("top"));
+        var width = parseFloat($(this).css("width"));
+        if (top <= -100 || width >= 150) {
+            $(this).remove();
+        }
+    });
+}, 500);
+
+showMessage();
+
+function clearMusicState() {
+    localStorage.removeItem('musicPlaying');
+    localStorage.removeItem('musicCurrentTime');
+}
+
+window.onload = function() {
+    clearMusicState(); 
+}
+
+function setupMusic() {
+    const music = document.getElementById('backgroundMusic');
+    
+    if (!localStorage.getItem('initialLoad')) {
+        clearMusicState();
+        localStorage.setItem('initialLoad', 'true');
+        music.currentTime = 0;
+    }
+
+    const isMusicPlaying = localStorage.getItem('musicPlaying') === 'true';
+    const musicCurrentTime = localStorage.getItem('musicCurrentTime') || 0;
+
+    if (isMusicPlaying) {
+        music.currentTime = parseFloat(musicCurrentTime);
+        music.play().catch(error => console.log('Playback failed', error));
+    }
+
+    music.addEventListener('play', () => {
+        localStorage.setItem('musicPlaying', 'true');
+    });
+
+    music.addEventListener('pause', () => {
+        localStorage.setItem('musicPlaying', 'false');
+    });
+
+    setInterval(() => {
+        localStorage.setItem('musicCurrentTime', music.currentTime);
+    }, 1000);
+
+    document.addEventListener('click', function startMusic() {
+        music.play().catch(error => {
+            console.log('Autoplay prevented', error);
+        });
+        document.removeEventListener('click', startMusic);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', setupMusic);
